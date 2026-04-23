bd79892c9faae391f9dc12b8ecb9774e62ddd9de
Tony Hall <tonyhall@Tonys-MacBook-Pro.local>
2026-04-23 09:38:46 -0400

    Fix SSL redirect loop for DigitalOcean deployment

--- episode_one/settings.py
+++ episode_one/settings.py
@@ -2,24 +2,18 @@ import os
 import sys
 import dotenv
 from pathlib import Path
-
 dotenv.load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 SECRET_KEY = os.environ.get('SECRET_KEY')
 if not SECRET_KEY:
     raise Exception("SECRET_KEY environment variable is not set")
-
 DEBUG = str(os.environ.get('DEBUG')) == "1"
-
 ALLOWED_HOSTS = []
 if not DEBUG:
     host = os.environ.get('DJANGO_ALLOWED_HOST')
     if not host:
         raise Exception("DJANGO_ALLOWED_HOST must be set in production")
     ALLOWED_HOSTS.append(host)
-
 INSTALLED_APPS = [
     'eo_app',
     'dal',
@@ -32,7 +26,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -42,9 +35,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'episode_one.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -60,13 +51,9 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'episode_one.wsgi.application'
-
 import dj_database_url
-
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
-
 if DEVELOPMENT_MODE:
     DATABASES = {
         "default": {
@@ -80,38 +67,30 @@ elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     DATABASES = {
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
     }
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
-
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/screenplays')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
 }
-
 from cdn.conf import * # noqa
 from cdn.backends import MediaRootS3Boto3Storage
-
 DEFAULT_FILE_STORAGE = "cdn.backends.MediaRootS3Boto3Storage"
 MEDIA_URL = 'https://episodeone.sfo3.digitaloceanspaces.com/screenplays/'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -119,12 +98,12 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
 # Security headers for production
 if not DEBUG:
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
     X_FRAME_OPTIONS = 'DENY'
-    SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT = False
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
